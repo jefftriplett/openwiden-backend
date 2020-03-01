b92e03602f79360deb19cc6d4407b78d7c882ccb
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-01 13:08:31 +0300

    :card_file_box: github_token field for user model added

--- openwiden/users/models.py
+++ openwiden/users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    github_token = models.CharField(max_length=40)
 
     def __str__(self):
         return self.username
