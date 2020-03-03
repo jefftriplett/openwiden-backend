de4fb1a9a98b6d2b3394185fc39c6e64923b56ca
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-03 23:20:04 +0300

    :sparkles: github oauth & user create
    :white_check_mark: added some tests for auth
    :arrow_up: PyGithub module added for working with GitHub API v3
    :clown_face: added mock for github get_user method

--- openwiden/users/test/test_views.py
+++ openwiden/users/test/test_views.py
@@ -13,10 +13,17 @@ from rest_framework.test import APITestCase
 
 from openwiden.users.test.factories import UserFactory
 from openwiden.users import github_oauth
+from django.utils.translation import gettext_lazy as _
 
 
 fake = Faker()
 User = get_user_model()
+
+
+class MockUser:
+    login = fake.word()
+    name = fake.name()
+    email = fake.email()
 
 
 class TestUserListTestCase(APITestCase):
@@ -32,16 +39,20 @@ class TestUserListTestCase(APITestCase):
         response: Response = self.client.post(self.url, {})
         eq_(response.status_code, status.HTTP_400_BAD_REQUEST, msg=response.data)
 
-    @mock.patch("openwiden.users.views.github.fetch_token")
-    def test_post_request_with_valid_data_success(self, patched):
-        patched.return_value = fake.pystr(min_chars=40, max_chars=40)
+    @mock.patch("openwiden.users.views.Github.get_user")
+    @mock.patch("openwiden.users.views.oauth.fetch_token")
+    def test_post_request_with_valid_data_success(
+        self, fetch_token_patched, get_user_patched
+    ):
+        fetch_token_patched.return_value = fake.pystr(min_chars=40, max_chars=40)
+        get_user_patched.return_value = MockUser()
         data = {"code": fake.pystr(min_chars=20, max_chars=20)}
-        response = self.client.post(self.url, data=data)
+        response: Response = self.client.post(self.url, data=data)
         user = User.objects.get(id=response.data.get("id"))
 
-        eq_(patched.call_count, 1)
+        eq_(fetch_token_patched.call_count, 1)
         eq_(response.status_code, status.HTTP_201_CREATED, msg=response.data)
-        eq_(user.username, self.user_data.get("username"))
+        eq_(user.login, response.data.get("login"))
         ok_(user.github_token)
 
 
@@ -56,8 +67,15 @@ class TestUserDetailTestCase(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION=f"Token {self.user.github_token}")
 
     def test_get_request_returns_user(self):
-        response = self.client.get(self.url)
+        response: Response = self.client.get(self.url)
         eq_(response.status_code, status.HTTP_200_OK)
+
+    def test_get_request_raises_expected_error(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token TEST")
+        response: Response = self.client.get(self.url)
+
+        eq_(response.status_code, status.HTTP_403_FORBIDDEN)
+        eq_(response.data, {"detail": _("Invalid token.")})
 
 
 class TestUserExtraActionTestCase(APITestCase):
@@ -70,11 +88,7 @@ class TestUserExtraActionTestCase(APITestCase):
 
     def test_get_auth_url_redirects_to_a_valid_url(self):
         response: HttpResponseRedirect = self.client.get(self.url)
-        github = github_oauth.GitHubOAuth(
-            settings.GITHUB_KEY, settings.GITHUB_SECRET, ["user:email"]
-        )
+        github = github_oauth.GitHubOAuth(**settings.GITHUB_OAUTH_SETTINGS)
         auth_url = github.authorization_url
 
-        self.assertRedirects(
-            response, auth_url, fetch_redirect_response=False,
-        )
+        self.assertRedirects(response, auth_url, fetch_redirect_response=False)
--- openwiden/users/views.py
+++ openwiden/users/views.py
@@ -3,17 +3,17 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.exceptions import APIException
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from openwiden.users import github_oauth
 from .models import User
 from .serializers import UserSerializer, GitHubOAuthCodeSerializer, CreateUserSerializer
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
+from github import Github
 
 
-github = github_oauth.GitHubOAuth(
-    settings.GITHUB_KEY, settings.GITHUB_SECRET, ["user:email"]
-)
+oauth = github_oauth.GitHubOAuth(**settings.GITHUB_OAUTH_SETTINGS)
 
 
 class GitHubOAuthAPIException(APIException):
@@ -44,18 +44,34 @@ class UserViewSet(viewsets.ModelViewSet):
             return [AllowAny()]
         return super().get_permissions()
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         code = serializer.validated_data["code"]
 
         try:
-            token = github.fetch_token(code)
+            token = oauth.fetch_token(code)
+            github = Github(token)
+            user = github.get_user()
         except github_oauth.GitHubOAuthException as e:
             raise GitHubOAuthAPIException(e)
 
-        create_serializer = CreateUserSerializer(data={"github_token": token})
-        create_serializer.is_valid()
-        return create_serializer.save()
+        data = {
+            "login": user.login,
+            "name": user.name,
+            "email": user.email,
+            "github_token": token,
+        }
+
+        serializer = CreateUserSerializer(data=data)
+        serializer.is_valid()
+        serializer.save()
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
     @action(detail=False, methods=["GET"], url_name="auth")
     def auth(self, request, *args, **kwargs):
-        return HttpResponseRedirect(github.authorization_url)
+        return HttpResponseRedirect(oauth.authorization_url)
