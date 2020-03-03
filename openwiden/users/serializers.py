f8aa8e5d9455270e972f837d0357704037e1c9e8
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-03 23:17:16 +0300

    :recycle: admin & user serializer refactor due model change

--- openwiden/users/serializers.py
+++ openwiden/users/serializers.py
@@ -5,13 +5,8 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = (
-            "id",
-            "username",
-            "first_name",
-            "last_name",
-        )
-        read_only_fields = ("username",)
+        fields = ("id", "login", "name", "email", "github_token")
+        read_only_fields = ("id",)
 
 
 class GitHubOAuthCodeSerializer(serializers.Serializer):
@@ -20,4 +15,11 @@ class GitHubOAuthCodeSerializer(serializers.Serializer):
 
 class CreateUserSerializer(UserSerializer):
     class Meta(UserSerializer.Meta):
-        fields = ("username", "first_name", "last_name", "github_token")
+        pass
+
+    def create(self, validated_data):
+        login = validated_data.pop("login")
+        instance, created = User.objects.update_or_create(
+            login=login, defaults=validated_data
+        )
+        return instance
