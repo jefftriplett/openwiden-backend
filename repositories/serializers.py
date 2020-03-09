f9186bf8063ffce55efc1371608ad8865a8de7d1
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-09 21:25:44 +0300

    :ok_hand: repository serializer fields added instead of __all__

--- repositories/serializers.py
+++ repositories/serializers.py
@@ -4,6 +4,19 @@ from .models import Repository
 
 
 class RepositorySerializer(serializers.ModelSerializer):
+    version_control_service_label = serializers.CharField(source="version_control_service.label")
+
     class Meta:
         model = Repository
-        fields = "__all__"
+        fields = (
+            "id",
+            "version_control_service_label",
+            "name",
+            "description",
+            "url",
+            "star_count",
+            "open_issues_count",
+            "forks_count",
+            "created_at",
+            "updated_at",
+        )
