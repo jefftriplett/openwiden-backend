13a3c99fe267470efcc32ad4895234c50ab2e105
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-09 21:29:20 +0300

    🚸 added OAuth2Token model admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import User
+from .models import User, OAuth2Token
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OAuth2Token)
+class OAuth2TokenAdmin(admin.ModelAdmin):
     pass
