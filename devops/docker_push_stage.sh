34e28ad48cf9f835c3fa2df95b4c8a40d62f8d80
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-13 23:52:27 +0300

    :construction_worker: CI improved

--- devops/docker_push_stage.sh
+++ devops/docker_push_stage.sh
@@ -2,5 +2,6 @@
 
 VERSION=$(python -c "import openwiden; print(openwiden.get_version())")
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USER" --password-stdin
-docker build -t stefanitsky/openwiden-backend:"$VERSION" .
+docker build -t stefanitsky/openwiden-backend:"$VERSION" -t stefanitsky/openwiden-backend:stage .
 docker push stefanitsky/openwiden-backend:"$VERSION"
+docker push stefanitsky/openwiden-backend:stage
