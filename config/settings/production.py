7a62f660f9c0c6132a758ed440d538d847b6f1f0
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-15 01:36:38 +0300

    :sparkles: sentry added for staging & production envs

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,12 @@
+from openwiden import get_version
 from .base import Base
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 
 class Production(Base):
 
+    DEBUG = Base.DEBUG
     INSTALLED_APPS = Base.INSTALLED_APPS
 
     # Site
@@ -21,3 +25,18 @@ class Production(Base):
 
     REST_FRAMEWORK = Base.REST_FRAMEWORK
     REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = ["rest_framework.renderers.JSONRenderer"]
+
+    sentry_init_kwargs = {
+        "dsn": Base.env("SENTRY_DSN"),
+        "integrations": [DjangoIntegration()],
+        "send_default_pii": True,
+    }
+    if not DEBUG:
+        sentry_init_kwargs.update(
+            {"release": f"openwiden-backend@{get_version()}", "environment": "production",}
+        )
+    else:
+        sentry_init_kwargs.update(
+            {"debug": True, "environment": "staging",}
+        )
+    sentry_sdk.init(**sentry_init_kwargs)
