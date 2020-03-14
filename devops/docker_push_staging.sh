1b511b24ef38c0ec0de236700e3b372dc9e028ac
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-14 17:10:26 +0300

    :whale: removed multiple compose files

--- devops/docker_push_staging.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VERSION=$(python -c "import openwiden; print(openwiden.get_version())")
-echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USER" --password-stdin
-docker build -t stefanitsky/openwiden-backend:"$VERSION" -t stefanitsky/openwiden-backend:staging .
-docker push stefanitsky/openwiden-backend:"$VERSION"
-docker push stefanitsky/openwiden-backend:staging
