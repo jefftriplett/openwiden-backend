e684612efa684cff79f67f2dd2d0ff1afa1e8c51
Alexandr <stefanitsky.mozdor@gmail.com>
2020-03-22 00:50:14 +0300

    :white_check_mark: repositories add view new tests added

--- repositories/tests/test_views.py
+++ repositories/tests/test_views.py
@@ -7,8 +7,8 @@ from rest_framework import status
 from rest_framework.reverse import reverse_lazy
 from rest_framework.test import APITestCase
 
+from repositories.exceptions import RepositoryURLParse, VersionControlServiceNotFound
 from repositories.tests.factories import RepositoryFactory
-
 
 fake = Faker()
 datetime_format = "%m/%d/%Y %I:%M %p"
@@ -54,3 +54,17 @@ class RepositoryViewSetTestCase(APITestCase):
             self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
             self.assertEqual(response.data["url"], url)
         self.assertEqual(RepositoryFactory._meta.model.objects.count(), len(git_urls))
+
+    @mock.patch("repositories.views.parse_repo_url")
+    def test_add_view_raises_repo_url_parse_error(self, patched_parse_repo_url):
+        patched_parse_repo_url.return_value = None
+        url = "https://github.com/golang/go"
+        response = self.client.post(reverse_lazy("repository-add"), data={"url": url})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"detail": RepositoryURLParse(url).detail})
+
+    def test_add_view_raises_vcs_not_found(self):
+        url = "https://example.com/golang/go"
+        response = self.client.post(reverse_lazy("repository-add"), data={"url": url})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"detail": VersionControlServiceNotFound("example.com").detail})
