afd975ae445de5245841ce9468e7eb2ae9def41d
Alexandr <stefanitsky.mozdor@gmail.com>
2020-04-03 13:37:06 +0300

    :white_check_mark: added simple test for OAuthCompleteFilter

---
+++ users/tests/test_filters.py
@@ -0,0 +1,24 @@
+from users.filters import OAuthCompleteFilter
+
+
+def test_oauth_complete_filter_get_schema_operation_parameters():
+    parameters = OAuthCompleteFilter().get_schema_operation_parameters(None)
+    expected = [
+        {
+            "name": "code",
+            "in": "query",
+            "required": True,
+            "description": "Received code for token exchange.",
+            "schema": {"type": "string",},
+        },
+        {
+            "name": "state",
+            "in": "query",
+            "required": True,
+            "description": (
+                "An unguessable random string. It is used to protect against cross-site request forgery attacks."
+            ),
+            "schema": {"type": "string",},
+        },
+    ]
+    assert parameters == expected
