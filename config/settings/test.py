4e84a48a385536ff70c9089e47e704b1f7bd6eb1
Alexandr <stefanitsky.mozdor@gmail.com>
2020-04-25 12:03:55 +0300

    :wrench: tests config configured for json format response

--- config/settings/test.py
+++ config/settings/test.py
@@ -8,3 +8,6 @@ class Test(Base):
     EMAIL_HOST = "localhost"
     EMAIL_PORT = 1025
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+    REST_FRAMEWORK = Base.REST_FRAMEWORK
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = ("rest_framework.renderers.JSONRenderer",)
