6663ae645992301ca131435c7731a7bd6da654a1
Alexandr <stefanitsky.mozdor@gmail.com>
2020-04-25 09:53:39 +0300

    :fire: verify JWT view removed

--- config/urls/v1/auth.py
+++ config/urls/v1/auth.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenRefreshView
 from openwiden.users import views
 
 urlpatterns = [
     path("login/<str:provider>/", views.OAuthLoginView.as_view(), name="login"),
     path("complete/<str:provider>/", views.OAuthCompleteView.as_view(), name="complete"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
