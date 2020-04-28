25c0f0756e33aab40f8201fa354172e72c0cda2f
Alexandr <stefanitsky.mozdor@gmail.com>
2020-04-28 21:13:56 +0300

    :white_check_mark: added tests prototypes for future use

--- openwiden/repositories/tests/test_services.py
+++ openwiden/repositories/tests/test_services.py
@@ -2,4 +2,21 @@ from django.test import TestCase
 
 
 class RepositoryServiceTestCase(TestCase):
-    pass
+    def test_update_user_repositories_cache(self):
+        # Pass user
+        # For each oauth token get raw json data for repositories
+        # Parse id and title + add provider from oauth token
+        # Save (cache) like list of repos: [{"id": 123, "name": "Test", "provider": "github"}]
+        self.skipTest("todo")
+
+    def test_repository_exist(self):
+        # Pass "id" and "provider"
+        # Get user's repositories cache
+        # Return True or False
+        self.skipTest("todo")
+
+    def test_add_repository(self):
+        # Get user's repositories cache
+        # Check if "id" and "provider" exists
+        # Call task for repository add
+        self.skipTest("todo")
