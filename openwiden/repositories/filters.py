d2b38489e49194c0127741710df29565a875ee62
Alexandr <stefanitsky.mozdor@gmail.com>
2020-05-03 20:28:53 +0300

    :art: star_count -> stars_count rename

--- openwiden/repositories/filters.py
+++ openwiden/repositories/filters.py
@@ -6,7 +6,7 @@ from openwiden.repositories import models
 class Repository(filters.FilterSet):
     version_control_service = filters.CharFilter(field_name="version_control_service", lookup_expr="host")
 
-    star_count_gte = filters.NumberFilter(field_name="star_count", lookup_expr="gte")
+    stars_count_gte = filters.NumberFilter(field_name="star_count", lookup_expr="gte")
     open_issues_count_gte = filters.NumberFilter(field_name="open_issues_count", lookup_expr="gte")
     forks_count_gte = filters.NumberFilter(field_name="forks_count", lookup_expr="gte")
 
@@ -19,7 +19,7 @@ class Repository(filters.FilterSet):
         model = models.Repository
         fields = (
             "version_control_service",
-            "star_count_gte",
+            "stars_count_gte",
             "open_issues_count_gte",
             "forks_count_gte",
             "created_at",
