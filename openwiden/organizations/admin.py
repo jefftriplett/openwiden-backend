86c0b44b4690fd7ee57160ac555ff2a7fcc6b239
Alexandr <stefanitsky.mozdor@gmail.com>
2020-05-04 23:25:43 +0300

    🚸 added inline members for admin

--- openwiden/organizations/admin.py
+++ openwiden/organizations/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from openwiden.organizations import models
 
 
+class MembersInline(admin.TabularInline):
+    model = models.Member
+
+
 @admin.register(models.Organization)
 class Organization(admin.ModelAdmin):
-    pass
+    inlines = (MembersInline,)
