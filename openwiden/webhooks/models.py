08adaf5e3e072eb2649318ce07dda5b4f482a0a1
Alexandr <stefanitsky.mozdor@gmail.com>
2020-05-10 16:59:26 +0300

    :card_file_box: repo webhook model changes

--- openwiden/webhooks/models.py
+++ openwiden/webhooks/models.py
@@ -1,17 +1,21 @@
 from django.db import models
+from django.urls import reverse
 from model_utils.models import UUIDModel
 from django.utils.translation import gettext_lazy as _
 from openwiden.repositories import models as repo_models
 
 
 class RepositoryWebhook(UUIDModel):
-    repository = models.OneToOneField(repo_models.Repository, models.CASCADE, verbose_name=_("repository"))
-    remote_id = models.PositiveIntegerField(_("remote id"))
+    repository = models.OneToOneField(
+        repo_models.Repository, models.CASCADE, related_name="webhook", verbose_name=_("repository")
+    )
+    remote_id = models.PositiveIntegerField(_("remote id"), blank=True, null=True)
 
-    url = models.URLField(_("url"))
+    url = models.URLField(_("url"), blank=True, null=True)
     secret = models.CharField(_("secret"), max_length=40)
 
-    issue_events_enabled = models.BooleanField(_("issue events enabled"))
+    is_active = models.BooleanField(_("is active"), default=False)
+    issue_events_enabled = models.BooleanField(_("issue events enabled"), default=False)
 
     created_at = models.DateTimeField(_("created at"), blank=True, null=True)
     updated_at = models.DateTimeField(_("updated at"), blank=True, null=True)
@@ -19,7 +23,9 @@ class RepositoryWebhook(UUIDModel):
     class Meta:
         verbose_name = _("webhook")
         verbose_name_plural = _("list of webhooks")
-        constraints = (models.UniqueConstraint(fields=("repository", "remote_id"), name="unique_repository_webhook"),)
 
     def __str__(self):
         return _("repository webhook for {repo}").format(repo=self.repository.name)
+
+    def get_absolute_url(self) -> str:
+        return reverse("api-v1:repo-webhook-receive", kwargs={"id": self.id})
