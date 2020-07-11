cd4b2332054f23f0715d282a94a9d4ef4264072a
Alexandr <stefanitsky.mozdor@gmail.com>
2020-07-11 18:17:23 +0300

    :white_check_mark: improved tests for users services

--- openwiden/users/tests/test_services.py
+++ openwiden/users/tests/test_services.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from unittest import mock
 
 import pytest
@@ -12,20 +13,29 @@ pytestmark = pytest.mark.django_db
 @mock.patch.object(services.RefreshToken, "for_user")
 def get_jwt_tokens(patched_refresh_token_for_user, mock_user, create_mock_refresh_token):
     expected = dict(access="12345", refresh="67890")
-
     patched_refresh_token_for_user.return_value = create_mock_refresh_token(**expected)
 
     assert services.get_jwt_tokens(mock_user) == expected
 
 
-def test_profile_cls_split_name_false(create_mock_profile):
-    profile = create_mock_profile(split_name=False)
+@pytest.mark.parametrize(
+    "split_name, name, expected",
+    [
+        pytest.param(True, "No Name", ("No", "Name"), id="split with name"),
+        pytest.param(False, "No Name", ("", ""), id="no split"),
+        pytest.param(True, None, ("", ""), id="split when name is None"),
+    ],
+)
+def test_profile_split_name_false(
+    create_mock_profile, split_name: bool, name: str, expected: Tuple[str],
+):
+    profile = create_mock_profile(split_name=split_name, name=name)
 
-    assert profile.first_name == ""
-    assert profile.last_name == ""
+    assert profile.first_name == expected[0]
+    assert profile.last_name == expected[1]
 
 
-def test_get_client(settings, authlib_settings_github, random_vcs):
+def test_get_client_exist(settings, authlib_settings_github, random_vcs):
     settings.AUTHLIB_OAUTH_CLIENTS = {random_vcs: authlib_settings_github}
     client = services.get_client(vcs=random_vcs)
 
@@ -38,25 +48,31 @@ def test_get_client_raises_error(settings):
         services.get_client(vcs="error")
 
 
-def test_update_token(random_vcs, fake_token, create_vcs_account):
-    access_token = "12345"
-    refresh_token = "67890"
-    vcs_account = create_vcs_account(vcs=random_vcs, access_token=access_token, refresh_token=refresh_token)
+@pytest.mark.parametrize(
+    "refresh_token, access_token",
+    [
+        pytest.param(None, None, id="nothing to update"),
+        pytest.param("access_token", None, id="access token"),
+        pytest.param(None, "refresh_token", id="refresh token"),
+        pytest.param("access_token", "refresh_token", id="access & refresh tokens"),
+    ],
+)
+def test_update_token(random_vcs, fake_token: dict, create_vcs_account, refresh_token: str, access_token: str):
+    factory_init_kwargs = {}
+    if refresh_token:
+        factory_init_kwargs["refresh_token"] = refresh_token
+    if access_token:
+        factory_init_kwargs["access_token"] = access_token
 
-    services.update_token(random_vcs, fake_token)
+    vcs_account = create_vcs_account(vcs=random_vcs, **factory_init_kwargs)
+    services.update_token(vcs=random_vcs, token=fake_token, refresh_token=refresh_token, access_token=access_token)
     vcs_account.refresh_from_db()
-    assert vcs_account.access_token != fake_token["access_token"]
-    assert vcs_account.refresh_token != fake_token["refresh_token"]
-    assert vcs_account.expires_at != fake_token["expires_at"]
 
-    services.update_token(random_vcs, fake_token, refresh_token=refresh_token)
-    vcs_account.refresh_from_db()
-    assert vcs_account.access_token == fake_token["access_token"]
-    assert vcs_account.refresh_token == fake_token["refresh_token"]
-    assert vcs_account.expires_at == fake_token["expires_at"]
-
-    services.update_token(random_vcs, fake_token, access_token=access_token)
-    vcs_account.refresh_from_db()
-    assert vcs_account.access_token == fake_token["access_token"]
-    assert vcs_account.refresh_token == fake_token["refresh_token"]
-    assert vcs_account.expires_at == fake_token["expires_at"]
+    if refresh_token or access_token:
+        assert vcs_account.refresh_token == fake_token["refresh_token"]
+        assert vcs_account.access_token == fake_token["access_token"]
+        assert vcs_account.expires_at == fake_token["expires_at"]
+    else:
+        assert vcs_account.access_token != fake_token["access_token"]
+        assert vcs_account.refresh_token != fake_token["refresh_token"]
+        assert vcs_account.expires_at != fake_token["expires_at"]
