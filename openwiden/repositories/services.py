850a6eb2c695abba506b3be7ebff1131f837105b
Alexandr <stefanitsky.mozdor@gmail.com>
2020-07-19 19:40:54 +0300

    :sparkles: added gitlab repositories sync

--- openwiden/repositories/services.py
+++ openwiden/repositories/services.py
@@ -2,7 +2,9 @@ from openwiden import enums, exceptions, vcs_clients
 from openwiden.repositories import models, error_messages
 from openwiden.users import models as users_models, selectors as users_selectors
 from openwiden.organizations import services as organization_services
+from openwiden.organizations import models as organizations_models
 from openwiden.vcs_clients.github.models import OwnerType
+from openwiden.vcs_clients.gitlab.models.repository import NamespaceKind
 from openwiden.webhooks import services as webhooks_services
 
 
@@ -51,6 +53,28 @@ def remove_repository(*, repository: models.Repository, user: users_models.User)
 
     repository.is_added = False
     repository.save(update_fields=("is_added",))
+
+
+def sync_user_repositories(*, vcs_account: users_models.VCSAccount, is_new_vcs_account: bool = False,) -> None:
+    # Init sync kwargs depends on new vcs account or not
+    sync_kwargs = {}
+    if is_new_vcs_account:
+        sync_kwargs.update(
+            {"sync_issues": False, "sync_programming_languages": False, "is_added": False,}
+        )
+
+    # Call sync depends on VCS
+    if vcs_account.vcs == enums.VersionControlService.GITHUB:
+        github_client = vcs_clients.GitHubClient(vcs_account)
+        user_repositories = github_client.get_user_repositories()
+        for repository in user_repositories:
+            sync_github_repository(
+                repository_id=repository.repository_id, github_client=github_client, **sync_kwargs,
+            )
+    elif vcs_account.vcs == enums.VersionControlService.GITLAB:
+        sync_gitlab_repositories(vcs_account=vcs_account)
+    else:
+        raise exceptions.ServiceException(f"vcs {vcs_account.vcs} is not supported yet!")
 
 
 def sync_github_repository(
@@ -129,21 +153,35 @@ def sync_github_repository_issues(*, repository: models.Repository, github_clien
         )
 
 
-def sync_user_repositories(*, vcs_account: users_models.VCSAccount, is_new_vcs_account: bool = False,) -> None:
-    # Init sync kwargs depends on new vcs account or not
-    sync_kwargs = {}
-    if is_new_vcs_account:
-        sync_kwargs.update(
-            {"sync_issues": False, "sync_programming_languages": False, "is_added": False,}
+def sync_gitlab_repositories(*, vcs_account: users_models.VCSAccount) -> None:
+    gitlab_client = vcs_clients.GitlabClient(vcs_account)
+    repositories = gitlab_client.get_user_repositories()
+
+    # Update or create each repository
+    for repository in repositories:
+        defaults = dict(
+            name=repository.name,
+            description=repository.description,
+            url=repository.web_url,
+            stars_count=repository.star_count,
+            open_issues_count=repository.open_issues_count,
+            forks_count=repository.forks_count,
+            created_at=repository.created_at,
+            updated_at=repository.last_activity_at,
+            visibility=repository.visibility,
         )
 
-    # Call sync depends on VCS
-    if vcs_account.vcs == enums.VersionControlService.GITHUB:
-        github_client = vcs_clients.GitHubClient(vcs_account)
-        user_repositories = github_client.get_user_repositories()
-        for repository in user_repositories:
-            sync_github_repository(
-                repository_id=repository.repository_id, github_client=github_client, **sync_kwargs,
+        # Add ownership
+        if repository.namespace.kind == NamespaceKind.ORGANIZATION:
+            defaults["organization"], _ = organizations_models.Organization.objects.get_or_create(
+                vcs=enums.VersionControlService.GITLAB,
+                remote_id=repository.namespace.namespace_id,
+                defaults=dict(name=repository.namespace.name),
             )
-    else:
-        raise exceptions.ServiceException(f"vcs {vcs_account.vcs} is not supported yet!")
+        else:
+            defaults["owner"] = vcs_account
+
+        # Update or create repository
+        models.Repository.objects.update_or_create(
+            remote_id=repository.repository_id, vcs=enums.VersionControlService.GITLAB, defaults=defaults,
+        )
