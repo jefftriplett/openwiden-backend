7bd56d6730f4141d6065fe16e5805e652d3740a2
Alexandr <stefanitsky.mozdor@gmail.com>
2020-07-19 16:10:31 +0300

    :fire: delete old tests and repository outdated services

--- openwiden/repositories/services.py
+++ openwiden/repositories/services.py
@@ -1,85 +1,9 @@
-import typing as t
-from datetime import datetime
-
-from django.db.models import QuerySet
-
 from openwiden import enums, exceptions, vcs_clients
 from openwiden.repositories import models, error_messages
 from openwiden.users import models as users_models, selectors as users_selectors
-from openwiden.organizations import models as organizations_models
 from openwiden.organizations import services as organization_services
 from openwiden.vcs_clients.github.models import OwnerType
 from openwiden.webhooks import services as webhooks_services
-
-
-class Repository:
-    @staticmethod
-    def sync(
-        vcs: str,
-        remote_id: int,
-        name: str,
-        url: str,
-        created_at: datetime,
-        updated_at: datetime,
-        description: str = None,
-        owner: users_models.User = None,
-        organization: organizations_models.Organization = None,
-        stars_count: int = 0,
-        open_issues_count: int = 0,
-        forks_count: int = 0,
-        programming_languages: dict = None,
-        visibility: str = enums.VisibilityLevel.private,
-    ) -> t.Tuple[models.Repository, bool]:
-        """
-        Synchronizes repository with specified data (update or create).
-        """
-        fields = dict(
-            name=name,
-            url=url,
-            description=description,
-            owner=owner,
-            organization=organization,
-            stars_count=stars_count,
-            open_issues_count=open_issues_count,
-            forks_count=forks_count,
-            programming_languages=programming_languages,
-            visibility=visibility,
-            created_at=created_at,
-            updated_at=updated_at,
-        )
-
-        # Try to create or update repository
-        try:
-            repository = models.Repository.objects.get(vcs=vcs, remote_id=remote_id)
-        except models.Repository.DoesNotExist:
-            repository = models.Repository.objects.create(vcs=vcs, remote_id=remote_id, is_added=False, **fields,)
-            created = True
-        else:
-            # Update values if repository exist
-            for k, v in fields.items():
-                setattr(repository, k, v)
-            repository.save(update_fields=fields.keys())
-            created = False
-
-        # TODO: notify
-        if created:
-            pass
-
-        return repository, created
-
-    @staticmethod
-    def added(visibility: str = enums.VisibilityLevel.public) -> QuerySet:
-        """
-        Returns repositories QuerySet filtered by "is_added" field and optional visibility (default is public).
-        """
-        return models.Repository.objects.filter(is_added=True, visibility=visibility)
-
-    @classmethod
-    def get_added_and_public_(cls) -> QuerySet:
-        """
-        Returns added and public visible repositories also known as "OpenWiden".
-        """
-        return cls.added(enums.VisibilityLevel.public)
 
 
 def delete_by_remote_id(*, remote_id: str):
