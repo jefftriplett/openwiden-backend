77f1e6df14a835f2a159084e70bdcc9e78120d0a
Alexandr <stefanitsky.mozdor@gmail.com>
2020-08-01 11:42:50 +0300

    :white_check_mark: add owner type cases (organization or user)

--- openwiden/tests/test_add_repository.py
+++ openwiden/tests/test_add_repository.py
@@ -10,6 +10,7 @@ from openwiden.enums import VersionControlService
 @pytest.mark.functional
 @pytest.mark.django_db
 @mock.patch("requests.sessions.Session.request")
+@pytest.mark.parametrize("is_organization_repository", [True, False])
 def test_github(
     mock_request,
     user,
@@ -17,32 +18,44 @@ def test_github(
     create_vcs_account,
     create_repository,
     create_mock_response,
+    github_organization_repository_json,
     github_repository_json,
     github_repository_languages_json,
     github_repository_issues_json,
     github_webhook_create_json,
     github_organization_json,
     github_user_membership_for_organization_json,
+    is_organization_repository: bool,
 ):
     api_client = create_api_client(user=user)
     vcs_account = create_vcs_account(user=user, vcs=VersionControlService.GITHUB)
     repository = create_repository(vcs=VersionControlService.GITHUB, owner=vcs_account, organization=None)
-
-    # Mock responses
-    mock_request.side_effect = [
+    mock_responses = [
         # Create mock repository response
-        create_mock_response(github_repository_json),
+        create_mock_response(
+            github_organization_repository_json if is_organization_repository else github_repository_json
+        ),
         # Create mock languages response
         create_mock_response(github_repository_languages_json),
-        # Create mock organization response
-        create_mock_response(github_organization_json),
-        # Create mock user membership check for organization
-        create_mock_response(github_user_membership_for_organization_json),
+    ]
+
+    if is_organization_repository:
+        mock_responses += [
+            # Create mock organization response
+            create_mock_response(github_organization_json),
+            # Create mock user membership check for organization
+            create_mock_response(github_user_membership_for_organization_json),
+        ]
+
+    mock_responses += [
         # Create mock repository issues response
         create_mock_response(github_repository_issues_json),
         # Create mock webhook create response
         create_mock_response(github_webhook_create_json),
     ]
+
+    # Mock responses
+    mock_request.side_effect = mock_responses
 
     # Make add request
     response = api_client.post(reverse("api-v1:user-repositories-add", kwargs={"id": str(repository.id)}))
@@ -55,6 +68,7 @@ def test_github(
 @pytest.mark.functional
 @pytest.mark.django_db
 @mock.patch("requests.sessions.Session.request")
+@pytest.mark.parametrize("is_organization_repository", [True, False])
 def test_gitlab(
     mock_request,
     user,
@@ -62,25 +76,36 @@ def test_gitlab(
     create_vcs_account,
     create_repository,
     create_mock_response,
+    gitlab_repository_json,
     gitlab_organization_repository_json,
     gitlab_repository_languages_json,
     gitlab_repository_issues_json,
     gitlab_webhook_create_json,
     gitlab_organization_json,
     gitlab_organization_member_json,
+    is_organization_repository: bool,
 ):
     api_client = create_api_client(user=user)
     vcs_account = create_vcs_account(user=user, vcs=VersionControlService.GITLAB)
     repository = create_repository(vcs=VersionControlService.GITLAB, owner=vcs_account, organization=None)
     mock_responses = [
         # Create mock repository
-        create_mock_response(gitlab_organization_repository_json),
+        create_mock_response(
+            gitlab_organization_repository_json if is_organization_repository else gitlab_repository_json
+        ),
         # Mock programming languages response
         create_mock_response(gitlab_repository_languages_json),
-        # Mock organization response
-        create_mock_response(gitlab_organization_json),
-        # Mock organization membership check
-        create_mock_response(gitlab_organization_member_json),
+    ]
+
+    if is_organization_repository:
+        mock_responses += [
+            # Mock organization response
+            create_mock_response(gitlab_organization_json),
+            # Mock organization membership check
+            create_mock_response(gitlab_organization_member_json),
+        ]
+
+    mock_responses += [
         # Mock issues response
         create_mock_response(gitlab_repository_issues_json),
         # Mock webhook create response
