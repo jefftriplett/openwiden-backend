57a7cb44ecc9cb1da98885c2f352d712ef93d740
Alexandr Stefanitsky-Mozdor <stefanitsky.mozdor@gmail.com>
2020-09-19 01:38:23 +0300

    :pencil: add error codes for swagger schema

--- openwiden/repositories/messages.py
+++ openwiden/repositories/messages.py
@@ -1,10 +1,10 @@
 from django.utils.translation import gettext_lazy as _
 
-REPOSITORY_DOES_NOT_EXIST = _("Repository with id {id} does not exist.")
 REPOSITORY_ALREADY_ADDED = _("Repository already added.")
 REPOSITORY_CANNOT_BE_ADDED_DUE_TO_STATE = _("Repository cannot be added due to {state} state.")
 REPOSITORY_ALREADY_REMOVED = _("Repository already removed.")
 NOT_ADDED_REPOSITORY_CANNOT_BE_REMOVED = _("Not added repository cannot be removed.")
+REPOSITORY_DOES_NOT_EXIST = _("Repository with id {id} does not exist.")
 
 REPOSITORY_IS_ADDED = _("Repository is added.")
 REPOSITORY_IS_REMOVED = _("Repository is removed.")
